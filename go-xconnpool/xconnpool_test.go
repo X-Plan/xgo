e973a1e5482ac1907ea1bb799362d640451fe78c
blinklv <blinklv@icloud.com>
2016-10-16 00:37:34 +0800

    go-xconnpool加入xconnpool_test.go文件

---
+++ go-xconnpool/xconnpool_test.go
@@ -0,0 +1,43 @@
+// xconnpool_test.go
+//
+//		Copyright (C), blinklv. All right reserved.
+//
+// 创建人: blinklv <blinklv@icloud.com>
+// 创建日期: 2016-10-15
+// 修订人: blinklv <blinklv@icloud.com>
+// 修订日期: 2016-10-16
+package xconnpool
+
+import (
+	"github.com/X-Plan/xgo/go-xassert"
+	"net"
+	"testing"
+)
+
+// 测试对panic的捕获.
+func TestPanic(t *testing.T) {
+	// 需要先开启server
+	xcp := New(10, func() (net.Conn, error) {
+		return net.Dial("tcp", "127.0.0.1:8000")
+	})
+	xassert.NotNil(t, xcp)
+
+	conn, err := xcp.Get()
+	xassert.IsNil(t, err)
+
+	// 先关闭连接池.
+	err = xcp.Close()
+	xassert.IsNil(t, err)
+
+	// 如下的两个操作都会触发
+	// panic, 但是现在转换为
+	// 错误返回.
+
+	// 重复关闭.
+	err = xcp.Close()
+	xassert.Equal(t, ErrClosed, err)
+
+	// 归还连接到已经关闭的连接池.
+	err = conn.Close()
+	xassert.Equal(t, ErrClosed, err)
+}
