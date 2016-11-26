8d332c501ac21bb761c648ab252c5d6125d9de16
blinklv <blinklv@icloud.com>
2016-11-26 20:59:03 +0800

    go-xkwfilter初步完成Filter接口.

--- go-xkwfilter/xkwfilter.go
+++ go-xkwfilter/xkwfilter.go
@@ -5,12 +5,13 @@
 // 创建人: blinklv <blinklv@icloud.com>
 // 创建日期: 2016-11-23
 // 修订人: blinklv <blinklv@icloud.com>
-// 修订日期: 2016-11-24
+// 修订日期: 2016-11-26
 
 // go-xkwfilter基于Aho-Corasick算法实现了一个关键字过滤器.
 package xkwfilter
 
 import (
+	"bufio"
 	"io"
 )
 
@@ -25,7 +26,7 @@ var (
 // XKeywordFilter一旦被创建就不能进行关键字的添加和
 // 删除.
 type XKeywordFilter struct {
-	mask string
+	mask []byte
 
 	// 并不是每个字节都会出现在关键字中,
 	// 因此对关键字中真正出现的字节进行
@@ -42,10 +43,15 @@ type XKeywordFilter struct {
 
 	// 失败回溯表.
 	fm []int
+
+	// 以字节为单位, 所有关键字和mask中最大
+	// 的长度.
+	maxlen int
 }
 
 func New(mask string, keywords ...string) *XKeywordFilter {
-	xkwf := &XKeywordFilter{mask: mask}
+	xkwf := &XKeywordFilter{mask: []byte(mask)}
+	xkwf.maxlen = len(xkwf.mask)
 
 	// cm中的值为-1表示该字节不存在.
 	for i := 0; i < 256; i++ {
@@ -58,6 +64,10 @@ func New(mask string, keywords ...string) *XKeywordFilter {
 	)
 	for _, kw := range keywords {
 		a := []byte(kw)
+		if len(a) > xkwf.maxlen {
+			xkwf.maxlen = len(a)
+		}
+
 		for _, c := range a {
 			if xkwf.cm[int(c)] == -1 {
 				xkwf.cm[int(c)] = n
@@ -73,36 +83,89 @@ func New(mask string, keywords ...string) *XKeywordFilter {
 // 的屏蔽字符串. 当出现EOF条件或出现错误的情况下该函数
 // 才会返回. 一个成功的返回必须要求err为nil, 而不是err
 // 为EOF, 因为Filter默认情况下就是要求r到达EOF.
-func (xkwf *XKeywordFilter) Filter(w io.Writer, r io.ByteReader) (int, error) {
+func (xkwf *XKeywordFilter) Filter(w io.Writer, r io.Reader) (n int, err error) {
 	var (
-		rerr, werr error
-		n          int
-		c          byte
-		s          = 0
+		c       byte
+		s, olds int
+		inc, nc int
+		br      = bufio.NewReader(r)
+		bw      = bufio.NewWriter(w)
+		buf     = make([]byte, 0, xkwf.maxlen)
+
+		// flag标识是否已经写入了mask.  Filter函数
+		// 在进行关键字过滤的时候遵循最长匹配的原则.
+		// 如果aab和aaab都是关键字的话, 会优先考虑
+		// aaab, 将其替换为mask. 如果多个关键字重叠
+		// 或者衔接在一起, 则会被替换为一个mask.
+		flag bool
 	)
 
+	defer bw.Flush()
+
+	// Dead: s == -1 Origin: s == 0 Active: s > 0
+	// 为了方便处理, 这里用s == -2表示Prepare状态.
+	s = -2
+
 	for {
-		if c, rerr = r.ReadByte(); rerr != nil && rerr != io.EOF {
-			return n, rerr
+		switch {
+		case s == -1:
+			// 执行了这步操作后s的取值只可能
+			// 大于等于0.
+			s, olds = xkwf.fm[olds], -1
+			continue
+
+		case s == 0:
+			flag = false
+
+			if olds == 0 {
+				if err = bw.WriteByte(c); err != nil {
+					return
+				}
+				n++
+			} else {
+				if inc, err = bw.Write(buf); err != nil {
+					return
+				}
+				n += inc
+				buf = buf[:0]
+				goto again
+			}
+
+		case s > 0:
+			if olds >= 0 {
+				if xkwf.am[s] {
+					if !flag {
+						// 只有上次写入非mask的情况下才可以
+						// 选择是否写入mask.
+						if inc, err = bw.Write(xkwf.mask); err != nil {
+							return
+						}
+						n += inc
+						flag = true
+					}
+					buf = buf[:0]
+				} else {
+					buf = append(buf, c)
+				}
+			} else {
+				goto again
+			}
+		case s == -2:
+			// 该状态放在最后的位置, 因为执行该语句
+			// 的可能性很小. 只有初始调用时才会执行.
+			s = 0
 		}
 
-		nc = xkwf.cm[int(c)]
-
-		for xkwf.transfer(s, nc) == -1 {
-			s = xkwf.fm[s]
-		}
-		s = xkwf.transfer(s, nc)
-
-		if xkwf.am[s] {
+		if c, err = br.ReadByte(); err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return
 		}
 
-		// 到达文件输入末尾, 退出循环.
-		if rerr == io.EOF {
-			break
-		}
+	again:
+		s, olds = xkwf.transfer(s, xkwf.cm[int(c)]), s
 	}
-
-	return n, nil
 }
 
 func (xkwf *XKeywordFilter) transfer(s int, nc int) int {
