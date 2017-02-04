93480cfc49e3e4a2bd90a2d2e4184861821409a2
blinklv <blinklv@icloud.com>
2017-02-04 23:57:16 +0800

    go-xconnpool: 1. 增加GetConnError类型.

--- go-xconnpool/xconnpool.go
+++ go-xconnpool/xconnpool.go
@@ -3,7 +3,7 @@
 // 创建人: blinklv <blinklv@icloud.com>
 // 创建日期: 2016-10-12
 // 修订人: blinklv <blinklv@icloud.com>
-// 修订日期: 2017-01-27
+// 修订日期: 2017-02-04
 
 // go-xconnpool实现了一个并发安全的连接池, 该连接池
 // 可以用来管理和重用连接, 由该连接池产生的连接满足
@@ -17,7 +17,7 @@ import (
 )
 
 // 版本信息
-var Version = "1.2.0"
+var Version = "1.2.1"
 
 // 如果连接池已经关闭却还对其进行操作
 // 会抛出该错误.
@@ -94,6 +94,18 @@ func (xc *XConn) Unuse() {
 // 一个十分简单但是通用的做法就是将net.Dial
 // 函数进行一次封装, 使其满足Factory的定义.
 type Factory func() (net.Conn, error)
+
+// 这是一个辅助类型, 它用于Factory在创建新连接
+// 失败后可以向外传递更多的信息.
+// NOTE: 不强制要求Factory一定使用该类型对error
+// 进行包装.
+type GetConnError struct {
+	error
+	// 当建立连接失败后可以将欲建立连接的ip地址
+	// 填写到该字段. 用于向调用方反馈存在问题的
+	// ip地址.
+	Addr string
+}
 
 // 连接池类型. 为了满足并发安全, 它在实现上
 // 用到了GO的原生Channel类型.
