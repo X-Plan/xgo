eade19567dd3d711e89496915ef679fd818cf9b4
blinklv <blinklv@icloud.com>
2017-03-10 19:13:17 +0800

    Complete 'newAddrUnit' function.

--- go-xsched/xsched.go
+++ go-xsched/xsched.go
@@ -45,6 +45,14 @@ func New(strs []string) (*XScheduler, error) {
 	}
 }
 
+const (
+	zeroInterval      = time.Duration(0)
+	minSampleInterval = 2 * time.Second
+	maxSampleInterval = 32 * time.Second
+	minWaitInterval   = 2 * time.Second
+	maxWaitInterval   = 128 * time.Second
+)
+
 type addrUnit struct {
 	address string
 	weight  int
@@ -63,13 +71,41 @@ func newAddrUnit(str string) *addrUnit {
 	var (
 		err  error
 		w    int64
-		u    = &addrUnit{}
 		strs = strings.Split(strings.TrimSpace(str), ":")
 	)
 
-	if len(strs) < 3 {
+	if len(strs) != 3 {
 		return nil
 	}
+
+	if w, err = strconv.ParseInt(strs[2], 10, 64); err != nil {
+		return nil
+	}
+
+	// Weight can't be negative, but it can be equal to zero.
+	if w < 0 {
+		return nil
+	}
+
+	// Just check whether the host/port field is valid.
+	if _, err = strconv.ParseUint(strs[1], 10, 16); err != nil {
+		return nil
+	}
+
+	if _, err = net.ResolveIPAddr("ip", strs[0]); err != nil {
+		return nil
+	}
+
+	u := &addrUnit{
+		address:        strs[0] + ":" + strs[1],
+		weight:         int(w),
+		available:      true,
+		sampleInterval: maxSampleInterval,
+		waitInterval:   minWaitInterval,
+		sampleTime:     time.Now().Add(maxSampleInterval),
+	}
+
+	return u
 }
 
 // Greatest common divisor.
