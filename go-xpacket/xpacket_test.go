5235b0a47f6664078b320a29558fac1dbb51a562
blinklv <blinklv@icloud.com>
2017-04-27 11:05:19 +0800

    Add Decode/Encode test.

--- go-xpacket/xpacket_test.go
+++ go-xpacket/xpacket_test.go
@@ -3,13 +3,15 @@
 // Author: blinklv <blinklv@icloud.com>
 // Create Time: 2017-01-25
 // Maintainer: blinklv <blinklv@icloud.com>
-// Last Change: 2017-04-12
+// Last Change: 2017-04-27
 package xpacket
 
 import (
 	"bytes"
 	"github.com/X-Plan/xgo/go-xassert"
 	"github.com/X-Plan/xgo/go-xrandstring"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -71,4 +73,30 @@ func testPacket(t *testing.T, n int, buf *bytes.Buffer) {
 func testFormat(t *testing.T, n int, buf *bytes.Buffer) {
 	xassert.IsNil(t, Encode(buf, []byte(xrandstring.Get(n))))
 	xassert.Match(t, buf.String(), `^SOP.+EOP$`)
+}
+
+// Reading the 'xpacket.input' file and encoding the content of it,
+// writing the result to 'xpacket.output' file.
+func TestEncode(t *testing.T) {
+	raw, err := ioutil.ReadFile("xpacket.input")
+	if err == nil {
+		ofile, err := os.OpenFile("xpacket.output", os.O_RDWR|os.O_CREATE, 0644)
+		if err == nil {
+			xassert.IsNil(t, Encode(ofile, raw))
+		}
+	}
+}
+
+// Reading the 'xpacket.output' file and decoding the content of it,
+// comparing its content with the content of 'xpacket.input' file.
+func TestDecode(t *testing.T) {
+	raw, err := ioutil.ReadFile("xpacket.input")
+	if err == nil {
+		ofile, err := os.Open("xpacket.output")
+		if err == nil {
+			out, err := Decode(ofile)
+			xassert.IsNil(t, err)
+			xassert.Equal(t, bytes.Compare(out, raw), 0)
+		}
+	}
 }
