0d8ac91a174ba751d9d6418f990f3e4679078572
blinklv <blinklv@icloud.com>
2017-08-04 11:08:35 +0800

    Change comment.

--- go-xconnpool/test/client/client.go
+++ go-xconnpool/test/client/client.go
@@ -1,16 +1,13 @@
 // client.go
 //
-// 创建人: blinklv <blinklv@icloud.com>
-// 创建日期: 2016-10-13
-// 修订人: blinklv <blinklv@icloud.com>
-// 修订日期: 2016-12-31
+// Authro: blinklv <blinklv@icloud.com>
+// Create Time: 2016-10-13
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2017-08-04
 
-// 该程序用于测试go-xconnpool包. 它是这个测试
-// 过程中的客户端, 用来向服务端发送请求. 每个
-// 客户端与服务端建立的一条连接都认为是一个次
-// 会话. 每次会话客户端发送若干条消息. 消息是
-// 普通的文本格式, 以换行符为分界线. 每次会话
-// 均是客户端主动关闭连接.
+// This program (client) is used to test go-xconnpool package. Each connection
+// is seen as a session, the client will send multiple plain text messages
+// separated by newline character. Client closes each session actively.
 package main
 
 import (
@@ -26,26 +23,14 @@ import (
 )
 
 var (
-	// 连接池的容量.
-	flagCapacity = flag.Int("capacity", 10, "capacity of connection pool")
-
-	// 服务端的主机地址.
-	flagHost = flag.String("host", "127.0.0.1", "host of server")
-
-	// 服务端的tcp端口号.
-	flagPort = flag.String("port", "8000", "server port")
-
-	// 会话次数.
+	flagCapacity      = flag.Int("capacity", 10, "capacity of connection pool")
+	flagHost          = flag.String("host", "127.0.0.1", "host of server")
+	flagPort          = flag.String("port", "8000", "server port")
 	flagSessionNumber = flag.Int("session-number", 10, "session number")
-
-	// 每次会话发送消息的次数.
-	flagMsgNumber = flag.Int("msg-number", 10, "message number per session")
-
-	// 测试次数
-	flagTestNumber = flag.Int("test-number", 3, "test number")
+	flagMsgNumber     = flag.Int("msg-number", 10, "message number per session")
+	flagTestNumber    = flag.Int("test-number", 3, "test number")
 )
 
-// 换行符.
 var newline = byte('\n')
 
 func main() {
@@ -59,12 +44,9 @@ func main() {
 		wg       = &sync.WaitGroup{}
 		err      error
 		addr     = *flagHost + ":" + *flagPort
-		// 当前的session number.
-		csn int32
+		csn      int32 // current session number
 	)
 
-	// 创建连接池. 工厂函数简单的使用
-	// net.Dial.
 	xcp := xconnpool.New(capacity, xconnpool.Factory(func() (conn net.Conn, err error) {
 		conn, err = net.Dial("tcp", addr)
 		return
@@ -78,10 +60,10 @@ func main() {
 	for j := 0; j < tn; j++ {
 		fmt.Fprintf(os.Stdout, "[Test: %d] Start\n", j)
 
-		// 创建会话.
+		// Create sessions
 		for i := 0; i < sn; i++ {
 			wg.Add(1)
-			// si(session index)为每个会话的编号.
+			// si (session index)
 			go func(si int) {
 				defer func() {
 					count := atomic.AddInt32(&csn, int32(-1))
@@ -92,7 +74,6 @@ func main() {
 				fmt.Fprintf(os.Stdout, "[Session: %d] Start\n", si)
 				count := atomic.AddInt32(&csn, int32(1))
 
-				// 从连接池中获取连接.
 				conn, err := xcp.Get()
 				if err != nil {
 					fmt.Fprintf(os.Stdout, "[Session: %d] Error Exit (%s)\n", err)
@@ -101,7 +82,6 @@ func main() {
 				defer conn.Close()
 				fmt.Fprintf(os.Stdout, "[State] Current Session Number: %d	XConnPool Size: %d\n", count, xcp.Size())
 
-				// 发送消息.
 				for i := 0; i < mn; i++ {
 					fmt.Fprintf(conn, "[Session: %d] %d\n", si, i)
 					r := bufio.NewReader(conn)
@@ -121,14 +101,9 @@ func main() {
 		fmt.Fprintf(os.Stdout, "[Test: %d] End\n", j)
 	}
 
-	// 这里进行延时3秒, 用来观测服务端的部分连接是
-	// 在客户端调用关闭连接池后才关闭的.
 	fmt.Fprintf(os.Stdout, "[INFO] Close XConnPool (But you have to wait 3s)\n")
 	time.Sleep(3 * time.Second)
 
-	// 应该在所有的会话都是释放连接后才关闭连接池.
-	// 若不满足该条件, 在XConn进行Close操作的时候
-	// 可能会失败(因为连接池已经关闭).
 	if err = xcp.Close(); err != nil {
 		fmt.Fprint(os.Stderr, "[ERROR]: %s\n", err)
 		os.Exit(1)
--- go-xconnpool/test/server/server.go
+++ go-xconnpool/test/server/server.go
@@ -1,12 +1,11 @@
 // server.go
 //
-// 创建人: blinklv <blinklv@icloud.com>
-// 创建日期: 2016-10-13
-// 修订人: blinklv <blinklv@icloud.com>
-// 修订日期: 2016-12-31
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2016-10-13
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2017-08-04
 
-// 该程序用于测试go-xconnpool包. 它是这个测试
-// 过程中的服务端, 用来回显客户端的请求.
+// This program (server) is used to test go-xconnpool package.
 package main
 
 import (
@@ -24,16 +23,14 @@ import (
 )
 
 var (
-	// 服务端的处理延时, 设置该项是为了更加方便的
-	// 观测连接的创建与关闭.
+	// The delay of handling requests in the server.
 	flagDelay = flag.Duration("delay", time.Second, "handle delay")
 
-	// TCP服务端口号.
+	// TCP listen port
 	flagPort = flag.String("port", "8000", "listen port")
 )
 
 var (
-	// 换行符.
 	newline = byte('\n')
 )
 
@@ -44,8 +41,7 @@ func main() {
 		delay = *flagDelay
 		port  = *flagPort
 		wg    = &sync.WaitGroup{}
-		// 用来记录当前的连接数.
-		total int32
+		total int32 // number of current connections
 	)
 
 	addr, err := net.ResolveTCPAddr("tcp", ":"+port)
@@ -63,7 +59,7 @@ func main() {
 	var (
 		conn net.Conn
 		sc   = make(chan os.Signal, 1)
-		cc   = make(chan net.Conn) // 无缓冲区
+		cc   = make(chan net.Conn)
 	)
 
 	go func() {
@@ -77,21 +73,12 @@ func main() {
 		}
 	}()
 
-	// 监听信号. 这样做的目的是希望
-	// 当服务收到这些信号的时候能
-	// 优雅的退出(将还未处理完的请求
-	// 处理完).
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
 
 	fmt.Fprintf(os.Stdout, "[INFO]: Start TCP Server\n")
-	// 连接处理循环.
 	for {
 		select {
-		// 信号事件与接收事件都不应该放在
-		// default范围内, 如果这样做其中
-		// 任何一个可能阻塞另一个的执行.
 		case sig := <-sc:
-			// 收到退出信号, 退出接收循环.
 			fmt.Fprintf(os.Stdout, "[INFO]: Receive %s Signal\n", sig)
 			goto exit
 		case conn = <-cc:
@@ -100,8 +87,6 @@ func main() {
 		}
 
 		wg.Add(1)
-		// 处理请求. conn参数时常变动, 因此
-		// 应该以参数的形式传递给处理函数.
 		go func(conn net.Conn) {
 			defer func() {
 				conn.Close()
@@ -117,10 +102,9 @@ func main() {
 					fmt.Fprintf(os.Stderr, "[ERROR]: %s\n", err)
 					return
 				} else if err == io.EOF {
-					// 客户端关闭连接, 退出.
+					// Client close the connection, so exit.
 					return
 				}
-				// 停顿一定的时间.
 				time.Sleep(delay)
 				io.WriteString(conn, line)
 			}
@@ -128,8 +112,7 @@ func main() {
 	}
 
 exit:
-	// 必须在所有连接已经处理完才会
-	// 退出服务.
+	// Exit after all connections have been finished.
 	wg.Wait()
 	fmt.Fprintf(os.Stdout, "[INFO]: Shutdown TCP Server\n")
 	os.Exit(1)
