1cf84d649789fc5fb82885b5db9c83ebca155b17
blinklv <blinklv@icloud.com>
2017-11-03 16:32:01 +0800

    Reconstruct the client.

--- go-xp/README.md
+++ go-xp/README.md
@@ -1,7 +1,6 @@
 # go-xp
 
 ![Building](https://img.shields.io/badge/building-passing-green.svg)
-![Version](https://img.shields.io/badge/version-1.0.0-blue.svg)
 ![License](https://img.shields.io/badge/license-MIT-blue.svg)
 
-**go-xp**提供了*X-Protocol*客户端和服务端的实现.
+**go-xp** implements the client and the server of *X-Protocol*.
--- go-xp/client.go
+++ go-xp/client.go
@@ -1,9 +1,9 @@
 // client.go
 //
-// 创建人: blinklv <blinklv@icloud.com>
-// 创建日期: 2017-02-06
-// 修订人: blinklv <blinklv@icloud.com>
-// 修订日期: 2017-02-09
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2017-11-03
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2017-11-03
 
 package xp
 
@@ -12,47 +12,104 @@ import (
 	"github.com/X-Plan/xgo/go-xconnpool"
 	"github.com/X-Plan/xgo/go-xpacket"
 	"github.com/X-Plan/xgo/go-xretry"
-	"github.com/X-Plan/xgo/go-xvalid"
+	"github.com/X-Plan/xgo/go-xscheduler"
 	"github.com/golang/protobuf/proto"
 	"net"
 	"sync/atomic"
 	"time"
 )
 
-type XScheduler interface {
-	Get() (string, error)
-	Feedback(string, bool)
+type Client struct {
+	// Scheduler is used to tell a client instance where is destination,
+	// so this field can't be empty. Its implementation must be satisfy
+	// 'xscheduler.Scheduler' interface, the more detail you can get from
+	// 'go-xscheduler' package.
+	Scheduler xscheduler.Scheduler
+
+	// The internal of a client will maintain a connection pool, so this
+	// field is used to specify the size of the pool. If this field is zero,
+	// using 32 by default, but this field can't be negative.
+	PoolSize int
+
+	seq uint64
+	xcp *xconnpool.XConnPool
 }
 
-type XClientConfig struct {
-	RetryCount   int           `xvalid:"min=0"`               // 重试次数.
-	Interval     time.Duration `xvalid:"min=0,default=100ms"` // 重试的基础时间间隔.
-	ConnPoolSize int           `xvalid:"min=0,default=16"`    // 连接池大小.
-	Scheduler    XScheduler    `xvalid:"noempty"`             // 地址调度器.
-}
+// Send a request to a server and receive the response. If "Sequence" field in
+// a request is empty, it will be filled automatically (this will change the
+// request argument, so you should be careful).
+func (client *Client) RoundTrip(req *Request) (*Response, error) {
 
-type XClient struct {
-	rc       int
-	interval time.Duration
-	seq      uint64
-	xcp      *xconnpool.XConnPool
-	sched    XScheduler
-}
+	// The first time we call this function, we need to init our client instance.
+	if client.xp == nil {
+		if err := client.init(); err != nil {
+			return nil, err
+		}
+	}
 
-func NewXClient(cfg *XClientConfig) (*XClient, error) {
-	err := xvalid.Validate(cfg)
+	if req.Head.Sequence == 0 {
+		req.Head.Sequence = atomic.AddUint64(&client.seq, uint64(1))
+	}
+
+	conn, err := client.xcp.Get()
+	if err != nil {
+		if fatal, ok := err.(xconnpool.GetConnError); ok {
+			client.Scheduler.Feedback(fatal.Addr, false)
+			return nil, fatal.Err
+		} else {
+			// I use 'xretry.FatalError' to wrap the raw error, this
+			// characteristic can be used by 'go-xretry' package, and
+			// it won't affect the raw error.
+			return nil, xretry.FatalError{err}
+		}
+	}
+	defer conn.Close()
+
+	data, err := proto.Marshal(req)
 	if err != nil {
 		return nil, err
 	}
 
-	xcli := &XClient{
-		rc:       cfg.RetryCount,
-		interval: cfg.Interval,
-		sched:    cfg.Scheduler,
+	if err = xpakcet.Encode(conn, data); err != nil {
+		client.release(conn)
+		return nil, err
 	}
 
-	xcli.xcp = xconnpool.New(cfg.ConnPoolSize, func() (net.Conn, error) {
-		addr, err := xcli.sched.Get()
+	if data, err = xpakcet.Decode(conn); err != nil {
+		client.release(conn)
+		return nil, err
+	}
+
+	rsp := &Response{}
+	if err = proto.Unmarshal(data, rsp); err != nil {
+		client.release(conn)
+		return nil, err
+	}
+
+	// If the error comes from a server end, we need to report it.
+	if rsp.GetRet().GetCode() == int32(Code_SERVER_ERROR) {
+		client.release(conn)
+		return nil, err
+	}
+
+	client.sched.Feedback(conn.RemoteAddr().String(), true)
+	return rsp, nil
+}
+
+func (client *Client) init() error {
+	if client.Scheduler == nil {
+		return fmt.Errorf("invalid scheduler field")
+	}
+
+	size := client.PoolSize
+	if size < 0 {
+		return fmt.Errorf("pool size (%d) can't be negative", size)
+	} else if size == 0 {
+		size = 32
+	}
+
+	client.xcp = xconnpool.New(size, func() (net.Conn, error) {
+		addr, err := client.Scheduler.Get()
 		if err != nil {
 			return nil, err
 		}
@@ -61,66 +118,12 @@ func NewXClient(cfg *XClientConfig) (*XClient, error) {
 		if err != nil {
 			return nil, xconnpool.GetConnError{Err: err, Addr: addr}
 		}
-
 		return conn, nil
 	})
-
-	return xcli, nil
+	return nil
 }
 
-// Request.Header中的Sequence字段如果未填, 则会使用
-// XClient自己默认的序列号.
-func (xcli *XClient) Send(req *Request) (*Response, error) {
-	if req.Head.Sequence == 0 {
-		req.Head.Sequence = atomic.AddUint64(&xcli.seq, uint64(1))
-	}
-	var rsp = &Response{}
-
-	err, _ := xretry.Retry(func() error {
-		conn, err := xcli.xcp.Get()
-		if err != nil {
-			if getConnErr, ok := err.(xconnpool.GetConnError); ok {
-				xcli.sched.Feedback(getConnErr.Addr, false)
-				return getConnErr.Err
-			} else {
-				return xretry.FatalError{err}
-			}
-		}
-		defer conn.Close()
-
-		data, err := proto.Marshal(req)
-		if err != nil {
-			return xretry.FatalError{err}
-		}
-
-		if err = xpacket.Encode(conn, data); err != nil {
-			xcli.sched.Feedback(conn.RemoteAddr().String(), false)
-			conn.(*xconnpool.XConn).Unuse()
-			return err
-		}
-
-		if data, err = xpacket.Decode(conn); err != nil {
-			xcli.sched.Feedback(conn.RemoteAddr().String(), false)
-			conn.(*xconnpool.XConn).Unuse()
-			return err
-		}
-
-		if err = proto.Unmarshal(data, rsp); err != nil {
-			xcli.sched.Feedback(conn.RemoteAddr().String(), false)
-			conn.(*xconnpool.XConn).Unuse()
-			return err
-		}
-
-		// 服务端错误需要上报.
-		if rsp.GetRet().GetCode() == int32(EnumRetCode_SERVER_ERROR) {
-			xcli.sched.Feedback(conn.RemoteAddr().String(), false)
-			conn.(*xconnpool.XConn).Unuse()
-			return fmt.Errorf("%s: %s", EnumRetCode_SERVER_ERROR, rsp.GetRet().GetMsg())
-		}
-
-		xcli.sched.Feedback(conn.RemoteAddr().String(), true)
-		return nil
-	}, xcli.rc, xcli.interval)
-
-	return rsp, err
+func (client *Client) release(conn net.Conn) {
+	client.Scheduler.Feedback(conn.RemoteAddr().String(), false)
+	conn.(*xconnpool.XConn).Unuse()
 }
